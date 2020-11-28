97ed37ff46660ee6e37345812da91473b022a066
Josh Gumerove <josh.gumerove@gmail.com>
2020-11-28 01:28:39 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -42,6 +42,7 @@ def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index,
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
-@assembled_matrix[0][2]= "Lemons"
-@assembled_matrix[0][2]
+@assembled_matrix[row][column]
+@assembled_matrix[row][column]= new_value
+@assembled_matrix[row][column]
 end
